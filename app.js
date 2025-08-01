16a0bf611f0fb0bdabd273241f5876ce68a865ed
NEGOSYO <158535439+NEGOSYO@users.noreply.github.com>
2025-08-01 22:54:14 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+const toggleButton = document.getElementById('toggleButton');
+
+// Hamburger menu toggle
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+// Change header background on scroll
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+// Close mobile menu on item click
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
+
+// Dark Mode Toggle Functionality
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        toggleButton.textContent = 'Switch to Light Mode';
+    } else {
+        toggleButton.textContent = 'Switch to Dark Mode';
+    }
+});
